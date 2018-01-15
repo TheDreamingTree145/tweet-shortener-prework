67f59ec1926071fb44a3d50fe0d6bed7db1b45ef
Dustin Anderson <dandersonsoftware@outlook.com>
2018-01-15 23:26:59 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -28,4 +28,13 @@ def bulk_tweet_shortener(tweets)
     puts word_substituter(tweet)
   end
 end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+    
   
