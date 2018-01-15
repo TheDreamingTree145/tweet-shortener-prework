ef3d9e1fe212b2553707c21d6d22c86256f17c01
Dustin Anderson <dandersonsoftware@outlook.com>
2018-01-15 23:24:49 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -23,5 +23,9 @@ def word_substituter(tweet)
   end.join(" ")
 end
 
-def bulk_tweet_shortener 
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
   
